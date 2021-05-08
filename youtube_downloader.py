c8fc71c2f0f9e0390a0934c2cf40c6a20b752760
AlexandraMoroiu <72220052+AlexandraMoroiu@users.noreply.github.com>
2021-05-08 21:55:19 +0300

    added download options

--- youtube_downloader.py
+++ youtube_downloader.py
@@ -1,8 +1,11 @@
 from pytube import YouTube
 import tkinter as tk
 from tkinter import filedialog
+from tkinter import ttk
+import os
 
 FOLDER_NAME = ""
+options = ["144p", "720p", "mp3"]
 
 WIDTH = 600
 HEIGHT = 400
@@ -13,30 +16,9 @@ BLACK = "#282828"
 
 TITLE = "YouTube Downloader"
 
-def findLocation():
+def find_location():
     global FOLDER_NAME
     FOLDER_NAME = tk.filedialog.askdirectory()
-
-#donwload video
-def DownloadVideo():
-    choice = ytdchoices.get()
-    url = ytdEntry.get()
-
-    if(len(url)>1):
-        ytdError.config(text="")
-        yt = YouTube(url)
-
-        if(choice == choices[0]):
-            select = yt.streams.filter(progressive=True).first()
-
-        elif(choice == choices[1]):
-            select = yt.streams.filter(progressive=True,file_extension='mp4').last()
-
-        elif(choice == choices[2]):
-            select = yt.streams.filter(only_audio=True).first()
-
-        else:
-            ytdError.config(text="Paste Link again!!",fg="red")
 
 class Youtube_Downloader:
     def __init__(self):
@@ -51,38 +33,58 @@ class Youtube_Downloader:
         self.name_label.grid(column = 0, row = 1)
         self.path_label = tk.Label(self.window, bg = RED, fg = BLACK, text = "File Path", font = ("Arial", 20, "bold"))
         self.path_label.grid(column = 0, row = 2)
-        self.ext_label = tk.Label(self.window, bg = RED, fg = BLACK, text = "File extension", font = ("Arial", 20, "bold"))
-        self.ext_label.grid(column = 0, row = 3)
+        self.option_box = ttk.Combobox(self.window, values = options)
+        self.option_box.grid(column = 0, row = 3)
 
         self.link_entry = tk.Entry(master = self.window, width = 60)
         self.link_entry.grid(column = 1, row = 0)
         self.name_entry = tk.Entry(master= self.window, width = 60)
         self.name_entry.grid(column = 1, row = 1)
-        self.saveEntry = tk.Button(self.window,height = 2, width=20,bg=WHITE,fg=BLACK,text="Choose Folder", font = ("Arial", 10, "bold"),command=findLocation)
+        self.saveEntry = tk.Button(self.window,height = 2, width=20,bg=WHITE,fg=BLACK,text="Choose Folder", font = ("Arial", 10, "bold"),command=find_location)
         self.saveEntry.grid(column = 1, row = 2)
-        self.ext_entry = tk.Entry(master = self.window, width = 60)
-        self.ext_entry.grid(column = 1, row = 3)
         self.download_button = tk.Button(self.window, text = "Download", command = self.__get_link)
         self.download_button.grid(column = 1, row = 4)
         return
 
-    def __downloader(self, link, save_path = "", save_name = "", extension = "mp4"):
-        
+    # choose video quality and download
+    def get_quality(self, link, save_path = "", save_name = ""):
+        nume = ""
+        choice = self.option_box.get()
         yt = YouTube(link)
-        print(link)
-        yt_stream = yt.streams.filter(progressive=True, file_extension=extension).order_by('resolution').desc().first()
-        yt_stream.download(output_path = save_path, filename = save_name)
 
-        return
+        if(choice == options[0]):
+            select = yt.streams.filter(progressive=True).first()
+            nume = select.download(output_path = save_path, filename = save_name)
+        
+
+        elif(choice == options[1]):
+            select = yt.streams.filter(progressive=True,file_extension='mp4').last()
+            nume = select.download(output_path = save_path, filename = save_name)
+        
+        elif(choice == options[2]):
+            select = yt.streams.filter(only_audio=True).first()
+            nume = select.download(output_path = save_path, filename = save_name)
+            base, extension = os.path.splitext(nume)
+            new = base + ".mp3"
+            os.rename(nume, new)
+
+    # def __downloader(self, link, save_path = "", save_name = ""):
+        
+    #     yt = YouTube(link)
+    #     yt_stream = yt.streams.filter(progressive=True, file_extension="mp4").order_by('resolution').desc().first()
+    #     yt_stream.download(output_path = save_path, filename = save_name)
+
+    #     return
 
   
     def __get_link(self):
         link = self.link_entry.get()
         path = FOLDER_NAME
         name = self.name_entry.get()
-        ext = self.ext_entry.get()
 
-        self.__downloader(link, path, name, ext)
+        print(name)
+
+        self.get_quality(link, path, name)
         
         return
 
