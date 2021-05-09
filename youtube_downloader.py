60061630e3291ad6c13432ca8b4d0b8267275318
AlexandraMoroiu <72220052+AlexandraMoroiu@users.noreply.github.com>
2021-05-09 18:46:00 +0300

    added comments and resolved syntax

--- youtube_downloader.py
+++ youtube_downloader.py
@@ -1,4 +1,3 @@
-  
 from pytube import YouTube
 import tkinter as tk
 from tkinter import filedialog
@@ -29,9 +28,9 @@ class Youtube_Downloader:
         self.window.title(TITLE)
         
         # centering the labels
-        self.window.columnconfigure(0,weight=1)
+        self.window.columnconfigure(0, weight = 1)
 
-        #added labels
+        # added labels
         self.link_label = tk.Label(self.window, bg = RED, fg = BLACK, text = "Youtube Link", font = ("Arial", 20, "bold"))
         self.link_label.grid(column = 0, row = 0)
         self.name_label = tk.Label(self.window, bg = RED, fg = BLACK, text = "Choose File name", font = ("Arial", 20, "bold"))
@@ -43,50 +42,54 @@ class Youtube_Downloader:
         self.option_box = ttk.Combobox(self.window, values = options)
         self.option_box.grid(column = 0, row = 7)
 
-        #receive input frum buttons/entries
+        # receive input from buttons/entries
         self.link_entry = tk.Entry(master = self.window, width = 60)
         self.link_entry.grid(column = 0, row = 1)
-        self.name_entry = tk.Entry(master= self.window, width = 60)
+        self.name_entry = tk.Entry(master = self.window, width = 60)
         self.name_entry.grid(column = 0, row = 3)
-        self.saveEntry = tk.Button(self.window,height = 2, width=20,bg=WHITE,fg=BLACK,text="Choose Folder", font = ("Arial", 10, "bold"),command=find_location)
+        self.saveEntry = tk.Button(self.window,height = 2, width = 20, bg = WHITE, fg = BLACK, text = "Choose Folder", font = ("Arial", 10, "bold"), command = find_location)
         self.saveEntry.grid(column = 0, row = 5)
         self.empty_space = tk.Label(self.window, bg = RED, fg = RED, text = "")
         self.empty_space.grid(column = 0, row = 8)
-        self.download_button = tk.Button(self.window,height = 2, width=20, text = "Download", command = self.__get_link)
+        self.download_button = tk.Button(self.window, height = 2, width = 20, text = "Download", command = self.__get_link)
         self.download_button.grid(column = 0, row = 9)
+
         return
 
-    # choose video quality and download
-    def get_quality(self, link, save_path = "", save_name = ""):
+    # choose format and download
+    def get_format(self, link, save_path = "", save_name = ""):
         choice = self.option_box.get()
         yt = YouTube(link)
+
+        # lowest video resolution
         if(choice == options[0]):
-            select = yt.streams.filter(progressive=True).get_lowest_resolution()
+            select = yt.streams.filter(progressive = True).get_lowest_resolution()
             select.download(output_path = save_path, filename = save_name)
         
-
+        # highest video resolution
         elif(choice == options[1]):
-            select = yt.streams.filter(progressive=True,file_extension='mp4').get_highest_resolution()
+            select = yt.streams.filter(progressive = True, file_extension = 'mp4').get_highest_resolution()
             select.download(output_path = save_path, filename = save_name)
         
         elif(choice == options[2]):
-            nume = ""
-            #if mp3 then change file extension
-            select = yt.streams.filter(only_audio=True).first()
-            nume = select.download(output_path = save_path, filename = save_name)
-            base, extension = os.path.splitext(nume)
+            name = ""
+            # if mp3 then change file extension
+            select = yt.streams.filter(only_audio = True).first()
+            name = select.download(output_path = save_path, filename = save_name)
+            base, extension = os.path.splitext(name)
             new = base + ".mp3"
-            os.rename(nume, new)
+            os.rename(name, new)
 
-    #download to wanted location
+    # download to wanted location
     def __get_link(self):
         link = self.link_entry.get()
         path = FOLDER_NAME
         name = self.name_entry.get()
-        self.get_quality(link, path, name)
+        self.get_format(link, path, name)
         
         return
-    #app loop
+
+    # app loop
     def run(self):
         self.window.mainloop()
         return
