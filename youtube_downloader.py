6d11d5ddf983a3a6b58ca711aa5ffceadc266ee4
lauratrandafir <73958287+lauratrandafir@users.noreply.github.com>
2021-05-10 22:35:20 +0300

    Add files via upload
    varianta finala

--- youtube_downloader.py
+++ youtube_downloader.py
@@ -3,14 +3,15 @@ import tkinter as tk
 from tkinter import filedialog
 from tkinter import ttk
 from tkinter import messagebox
-import requests
+from tkinter import *
+from PIL import Image, ImageTk
 import os
 
 FOLDER_NAME = ""
 options = ["lowest", "highest", "mp3"]
 
-WIDTH = 500
-HEIGHT = 400
+WIDTH = 600
+HEIGHT = 450
 
 WHITE = "#ffffff"
 RED = "#c4302b"
@@ -28,35 +29,93 @@ class Youtube_Downloader:
         self.window.geometry("{}x{}".format(WIDTH, HEIGHT))
         self.window.configure(bg = RED)
         self.window.title(TITLE)
-        
         # centering the labels
         self.window.columnconfigure(0, weight = 1)
 
         # added labels
-        self.link_label = tk.Label(self.window, bg = RED, fg = BLACK, text = "Youtube Link", font = ("Arial", 20, "bold"))
-        self.link_label.grid(column = 0, row = 0)
-        self.name_label = tk.Label(self.window, bg = RED, fg = BLACK, text = "Choose File name", font = ("Arial", 20, "bold"))
-        self.name_label.grid(column = 0, row = 2)
-        self.path_label = tk.Label(self.window, bg = RED, fg = BLACK, text = "File Path", font = ("Arial", 20, "bold"))
-        self.path_label.grid(column = 0, row = 4)
-        self.path_label = tk.Label(self.window, bg = RED, fg = BLACK, text = "Download Options", font = ("Arial", 20, "bold"))
-        self.path_label.grid(column = 0, row = 6)
-        self.option_box = ttk.Combobox(self.window, values = options)
-        self.option_box.grid(column = 0, row = 7)
+
+        # label for Youtube logo
+        self.youtube = tk.PhotoImage(file='youtube.png')
+        self.youtube_image = tk.Label(self.window, image = self.youtube, bd=0, highlightthickness=0)
+        self.youtube_image["bg"] = RED
+        self.youtube_image["border"] = "0"
+        self.youtube_image.grid(column = 0, row = 2)
+
+        # enter link label photo
+        self.enter_link_label = tk.PhotoImage(file='enter_link.png')
+        self.link_label = tk.Label(self.window, image = self.enter_link_label, bd=0, highlightthickness=0)
+        self.link_label["bg"] = RED
+        self.link_label["border"] = "0"
+        self.link_label.grid(column = 0, row = 3)
+    
+        # enter label photo
+        self.enter_name_label = tk.PhotoImage(file='enter_name.png')
+        self.name_label = tk.Label(self.window, image = self.enter_name_label, bd=0, highlightthickness=0)
+        self.name_label["bg"] = RED
+        self.name_label["border"] = "0"
+        self.name_label.grid(column = 0, row = 5)
+    
+        # location label with photo
+        self.location_name_label = tk.PhotoImage(file='location_name.png')
+        self.location_label = tk.Label(self.window, image = self.location_name_label, bd=0, highlightthickness=0)
+        self.location_label["bg"] = RED
+        self.location_label["border"] = "0"
+        self.location_label.grid(column = 0, row = 7)
+    
+        # path label with photo
+        self.path_name_label = tk.PhotoImage(file='path_name.png')
+        self.path_label = tk.Label(self.window, image = self.path_name_label, bd=0, highlightthickness=0)
+        self.path_label["bg"] = RED
+        self.path_label["border"] = "0"
+        self.path_label.grid(column = 0, row = 9)
 
         # receive input from buttons/entries
-        self.link_entry = tk.Entry(master = self.window, width = 60)
-        self.link_entry.grid(column = 0, row = 1)
-        self.name_entry = tk.Entry(master = self.window, width = 60)
-        self.name_entry.grid(column = 0, row = 3)
-        self.saveEntry = tk.Button(self.window,height = 2, width = 20, bg = WHITE, fg = BLACK, text = "Choose Folder", font = ("Arial", 10, "bold"), command = find_location)
-        self.saveEntry.grid(column = 0, row = 5)
+
+        # help button image
+        self.help_btn = PhotoImage(file='help.png')
+        self.help_button = tk.Button(self.window, image = self.help_btn, command = self.get_help, bd=0, highlightthickness=0)
+        self.help_button["bg"] = RED
+        self.help_button["border"] = "0"
+        self.help_button.grid(column = 1, row = 0)
+    
+        # display empty space
         self.empty_space = tk.Label(self.window, bg = RED, fg = RED, text = "")
-        self.empty_space.grid(column = 0, row = 8)
-        self.download_button = tk.Button(self.window, height = 2, width = 20, text = "Download", command = self.get_link)
-        self.download_button.grid(column = 0, row = 9)
+        self.empty_space.grid(column = 0, row = 1)
+
+        # display entry for entering link
+        self.link_entry = tk.Entry(master = self.window, width = 60)
+        self.link_entry.grid(column = 0, row = 4)
+    
+        # display entry for entering file name
+        self.name_entry = tk.Entry(master = self.window, width = 60)
+        self.name_entry.grid(column = 0, row = 6)
+
+        # display button image for choosing folder
+        self.choose_folder_btn = PhotoImage(file='choose_folder.png')
+        self.save_entry = tk.Button(self.window, image = self.choose_folder_btn, command = find_location, bd=0, highlightthickness=0)
+        self.save_entry["bg"] = RED
+        self.save_entry["border"] = "0"
+        self.save_entry.grid(column = 0, row = 8)
+    
+        # display option box for entering quality of mp3 options
+        self.option_box = ttk.Combobox(self.window, values = options)
+        self.option_box.grid(column = 0, row = 10)
+
+        self.empty_space = tk.Label(self.window, bg = RED, fg = RED, text = "")
+        self.empty_space.grid(column = 0, row = 11)
+
+        self.download_btn = tk.PhotoImage(file='download.png')
+        self.download_button = tk.Button(self.window, image = self.download_btn, command = self.get_link, bd=0, highlightthickness=0)
+        self.download_button["bg"] = RED
+        self.download_button["border"] = "0"
+        self.download_button.grid(column = 0, row = 12)
 
         return
+    
+    # show help message box
+    def get_help(self):
+        messagebox.showinfo(title="Help",
+        message="Hello! In order to download from YouTube, paste a valid Youtube link, choose a name for your file and a location, then choose the format and you're good to go!") 
     
     # choose format and download
     def get_format(self, link, save_path = "", save_name = ""):
@@ -88,10 +147,10 @@ class Youtube_Downloader:
             new = base + ".mp3"
             os.rename(name, new)
 
-    # check if the tumbnail of a  youtube link exists and if it doesn't, return False
+    # check if the thumbnail of a  youtube link exists and if it doesn't, return False
     def is_valid_link(self, link):
         try:
-            tumbnail_url = YouTube(link).thumbnail_url
+            thumbnail = YouTube(link).thumbnail_url
             return True
         except Exception:
             return False
@@ -110,7 +169,7 @@ class Youtube_Downloader:
             messagebox.showerror('Required Fields', 'Please provide Youtube Link!')
             return
 
-        # if the user did not introduce a valid toutube link, show error box
+        # if the user did not introduce a valid youtube link, show error box
         if self.is_valid_link(link) == False:
             messagebox.showerror('Required Fields', 'Please provide a valid Youtube Link!')
             return
@@ -124,6 +183,7 @@ class Youtube_Downloader:
     # app loop
     def run(self):
         self.window.mainloop()
+
         return
 
 def main():
